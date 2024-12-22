1d56b5c662ec8308641008cfa9451822e66b0b39
singhjatin297 <singhjatin297@gmail.com>
2024-12-23 00:44:05 +0530

    Stop tracking dist folder

--- backend/dist/index.js
+++
@@ -1,9 +0,0 @@
-import { IoManager } from "./managers/IoManager.js";
-import { UserManager } from "./managers/UserManager.js";
-const io = IoManager.getIo();
-io.listen(3000);
-const userManager = new UserManager();
-io.on("connection", (socket) => {
-    console.log("Server Connected");
-    userManager.addUser(socket);
-});
